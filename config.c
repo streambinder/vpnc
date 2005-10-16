916b68271581777b98ac3b23471a7ce984f16fda
massar@UNIX-AG.UNI-KL.DE <massar@UNIX-AG.UNI-KL.DE@315857ad-0bdb-0310-b42e-dec37551a5f0>
2005-10-16 20:49:39 +0000

    add support for obfuscated passwords (thanks to HAL-9000@evilscientists.de)
    git-svn-id: http://svn.unix-ag.uni-kl.de/vpnc/trunk@63 315857ad-0bdb-0310-b42e-dec37551a5f0

--- config.c
+++ config.c
@@ -26,6 +26,8 @@
 #include <errno.h>
 #include <sys/utsname.h>
 
+#include <gcrypt.h>
+
 #include "sysdep.h"
 #include "config.h"
 #include "vpnc.h"
@@ -86,6 +88,130 @@ void hex_dump(const char *str, const void *data, ssize_t len)
 		printf("%02x", p[i]);
 	}
 	printf("\n");
+}
+
+static int hex2bin_c(unsigned int c)
+{
+	if ((c >= '0')&&(c <= '9'))
+		return c - '0';
+	if ((c >= 'A')&&(c <= 'F'))
+		return c - 'A' + 10;
+	if ((c >= 'a')&&(c <= 'f'))
+		return c - 'a' + 10;
+	return -1;
+}
+
+static int hex2bin(const char *str, char **bin, int *len)
+{
+	char *p;
+	int i, l;
+	
+	if (!bin)
+		return EINVAL;
+	
+	for (i = 0; str[i] != '\0'; i++)
+		if (hex2bin_c(str[i]) == -1)
+			return EINVAL;
+	
+	l = i;
+	if ((l & 1) != 0)
+		return EINVAL;
+	l /= 2;
+	
+	p = malloc(l);
+	if (p == NULL)
+		return ENOMEM;
+	
+	for (i = 0; i < l; i++)
+		p[i] = hex2bin_c(str[i*2]) << 4 | hex2bin_c(str[i*2+1]);
+	
+	*bin = p;
+	if (len)
+		*len = l;
+	
+	return 0;
+}
+
+static int deobfuscate(char *ct, int len, const char **resp, char *reslenp)
+{
+	const char *h1  = ct;
+	const char *h4  = ct + 20;
+	const char *enc = ct + 40;
+	
+	char ht[20], h2[20], h3[20], key[24];
+	const char *iv = h1;
+	char *res;
+	gcry_cipher_hd_t ctx;
+	int reslen;
+	
+	if (len < 48)
+		return -1;
+	len -= 40;
+	
+	memcpy(ht, h1, 20);
+	
+	ht[19]++;
+	gcry_md_hash_buffer(GCRY_MD_SHA1, h2, ht, 20);
+	
+	ht[19] += 2;
+	gcry_md_hash_buffer(GCRY_MD_SHA1, h3, ht, 20);
+	
+	memcpy(key, h2, 20);
+	memcpy(key+20, h3, 4);
+	/* who cares about parity anyway? */
+	
+	gcry_md_hash_buffer(GCRY_MD_SHA1, ht, enc, len);
+	
+	if (memcmp(h4, ht, 20) != 0)
+		return -1;
+	
+	res = malloc(len);
+	if (res == NULL)
+		return -1;
+	
+	gcry_cipher_open(&ctx, GCRY_CIPHER_3DES, GCRY_CIPHER_MODE_CBC, 0);
+	gcry_cipher_setkey(ctx, key, 24);
+	gcry_cipher_setiv(ctx, iv, 8);
+	gcry_cipher_decrypt(ctx, (unsigned char *)res, len, (unsigned char *)enc, len);
+	gcry_cipher_close(ctx);
+	
+	reslen = len - res[len-1];
+	res[reslen] = '\0';
+	
+	if (resp)
+		*resp = res;
+	if (reslenp)
+		*reslenp = reslen;
+	return 0;
+}
+
+static void config_deobfuscate(int obfuscated, int clear)
+{
+	int ret, len = 0;
+	char *bin = NULL;
+	
+	if (config[obfuscated] == NULL)
+		return;
+	
+	if (config[clear] != NULL) {
+		config[obfuscated] = NULL;
+		error(0, 0, "warning: ignoring obfuscated password because cleartext password set");
+		return;
+	}
+	
+	ret = hex2bin(config[obfuscated], &bin, &len);
+	if (ret != 0) {
+		error(1, 0, "error: deobfuscating of password failed (input not a hex string)");
+	}
+	
+	ret = deobfuscate(bin, len, config+clear, NULL);
+	free(bin);
+	if (ret != 0) {
+		error(1, 0, "error: deobfuscating of password failed");
+	}
+	
+	config[obfuscated] = NULL;
+	return;
 }
 
 static const char *config_def_description(void)
@@ -173,7 +299,14 @@ static const struct config_names_s {
 		NULL,
 		"IPSec secret ",
 		"<ASCII string>",
-		"your group password (cleartext, no support for obfuscated strings)",
+		"your group password (cleartext)",
+		NULL
+	}, {
+		CONFIG_IPSEC_SECRET_OBF, 1, 1,
+		NULL,
+		"IPSec obfuscated secret ",
+		"<hex string>",
+		"your group password (obfuscated)",
 		NULL
 	}, {
 		CONFIG_XAUTH_USERNAME, 1, 0,
@@ -187,7 +320,14 @@ static const struct config_names_s {
 		NULL,
 		"Xauth password ",
 		"<ASCII string>",
-		"your password (cleartext, no support for obfuscated strings)",
+		"your password (cleartext)",
+		NULL
+	}, {
+		CONFIG_XAUTH_PASSWORD_OBF, 1, 1,
+		NULL,
+		"Xauth obfuscated password ",
+		"<hex string>",
+		"your password (obfuscated)",
 		NULL
 	}, {
 		CONFIG_UDP_ENCAP, 0, 0,
@@ -542,7 +682,10 @@ void do_config(int argc, char **argv)
 		fprintf(stderr,
 			"WARNING! active debug level is >= 99, output includes username and password (hex encoded)\n");
 	}
-
+	
+	config_deobfuscate(CONFIG_IPSEC_SECRET_OBF, CONFIG_IPSEC_SECRET);
+	config_deobfuscate(CONFIG_XAUTH_PASSWORD_OBF, CONFIG_XAUTH_PASSWORD);
+	
 	for (i = 0; i < LAST_CONFIG; i++) {
 		if (config[i] != NULL || config[CONFIG_NON_INTERACTIVE] != NULL)
 			continue;
@@ -583,7 +726,7 @@ void do_config(int argc, char **argv)
 		case CONFIG_XAUTH_USERNAME:
 			getline(&s, &s_len, stdin);
 		}
-		if (s != NULL && s[strlen(s) - 1] == '\n')
+		if (s != NULL && strlen(s) > 0 && s[strlen(s) - 1] == '\n')
 			s[strlen(s) - 1] = 0;
 		config[i] = s;
 	}
