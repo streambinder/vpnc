576aad0f2d110ea772217693570bbbbd03633310
Maurice Massar <Maurice Massar@315857ad-0bdb-0310-b42e-dec37551a5f0>
2011-01-26 17:11:51 +0000

    Jindrich Makovicka <makovick@gmail.com>:
    [Commit of fix for issue 1]
    
    I had some trouble with a concentrator, which seems to send weird
    isakmp delete payloads. The SPI specified in the delete payload does
    not match the current transmit SPI, and when the packet is just
    ignored, everything is fine.
    
    However, as the current vpnc accepts this packet, two things happen:
    
    1) do_phase2_qm opens a new socket, which is unknown to the main loop
    doing select. It means that vpnc will get stuck in a busy loop because
    there are still data on the old socket, but they are not read anymore,
    and the new socket is not select()ed at all.
    
    2) if the above issue is fixed, vpnc still eventually fails with "HMAC
    mismatch in ESP mode".
    
    The attached patches fix these two issued by skipping opening of a new
    esp socket when there already is one, and by ignoring delete payloads
    with spi other than our current tx.spi .
    
    
    git-svn-id: http://svn.unix-ag.uni-kl.de/vpnc/trunk@453 315857ad-0bdb-0310-b42e-dec37551a5f0

--- vpnc.c
+++ vpnc.c
@@ -2779,32 +2779,34 @@ static void do_phase2_qm(struct sa_block *s)
 		free(dh_shared_secret);
 		free_isakmp_packet(r);
 
-		if ((opt_natt_mode == NATT_CISCO_UDP) && s->ipsec.peer_udpencap_port) {
-			s->esp_fd = make_socket(s, opt_udpencapport, s->ipsec.peer_udpencap_port);
-			s->ipsec.encap_mode = IPSEC_ENCAP_UDP_TUNNEL;
-			s->ipsec.natt_active_mode = NATT_ACTIVE_CISCO_UDP;
-		} else if (s->ipsec.encap_mode != IPSEC_ENCAP_TUNNEL) {
-			s->esp_fd = s->ike_fd;
-		} else {
+		if (s->esp_fd == 0) {
+			if ((opt_natt_mode == NATT_CISCO_UDP) && s->ipsec.peer_udpencap_port) {
+				s->esp_fd = make_socket(s, opt_udpencapport, s->ipsec.peer_udpencap_port);
+				s->ipsec.encap_mode = IPSEC_ENCAP_UDP_TUNNEL;
+				s->ipsec.natt_active_mode = NATT_ACTIVE_CISCO_UDP;
+			} else if (s->ipsec.encap_mode != IPSEC_ENCAP_TUNNEL) {
+				s->esp_fd = s->ike_fd;
+			} else {
 #ifdef IP_HDRINCL
-			int hincl = 1;
+				int hincl = 1;
 #endif
 
-			s->esp_fd = socket(PF_INET, SOCK_RAW, IPPROTO_ESP);
-			if (s->esp_fd == -1) {
-				close_tunnel(s);
-				error(1, errno, "Couldn't open socket of ESP. Maybe something registered ESP already.\nPlease try '--natt-mode force-natt' or disable whatever is using ESP.\nsocket(PF_INET, SOCK_RAW, IPPROTO_ESP)");
-			}
+				s->esp_fd = socket(PF_INET, SOCK_RAW, IPPROTO_ESP);
+				if (s->esp_fd == -1) {
+					close_tunnel(s);
+					error(1, errno, "Couldn't open socket of ESP. Maybe something registered ESP already.\nPlease try '--natt-mode force-natt' or disable whatever is using ESP.\nsocket(PF_INET, SOCK_RAW, IPPROTO_ESP)");
+				}
 #ifdef FD_CLOEXEC
-			/* do not pass socket to vpnc-script, etc. */
-			fcntl(s->esp_fd, F_SETFD, FD_CLOEXEC);
+				/* do not pass socket to vpnc-script, etc. */
+				fcntl(s->esp_fd, F_SETFD, FD_CLOEXEC);
 #endif
 #ifdef IP_HDRINCL
-			if (setsockopt(s->esp_fd, IPPROTO_IP, IP_HDRINCL, &hincl, sizeof(hincl)) == -1) {
-				close_tunnel(s);
-				error(1, errno, "setsockopt(esp_fd, IPPROTO_IP, IP_HDRINCL, 1)");
-			}
+				if (setsockopt(s->esp_fd, IPPROTO_IP, IP_HDRINCL, &hincl, sizeof(hincl)) == -1) {
+					close_tunnel(s);
+					error(1, errno, "setsockopt(esp_fd, IPPROTO_IP, IP_HDRINCL, 1)");
+				}
 #endif
+			}
 		}
 
 		s->ipsec.rx.seq_id = s->ipsec.tx.seq_id = 1;
