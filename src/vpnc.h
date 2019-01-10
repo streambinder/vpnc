074e1cebd5468bacaff36a5d94eb2bfcab30cc80
Ralph Schmieder <ralph.schmieder@gmail.com>
2019-01-09 16:56:07 -0800

    patch for IKE phase 1 rekey, QoS and IKE keepalives
    please find below a patch that addresses three things in vpnc:
    
    1) added IKE phase 1 rekeying. This essentially uses an older patch from this list (http://lists.unix-ag.uni-kl.de/pipermail/vpnc-devel/2010-March/003442.html). I did some changes to that patch so that it can be applied cleanly to the current source tree and made sure the corresponding phase1 rekey is actually added at the right place within vpnc_main_loop in tunip.c
    
    2) added QoS handling for UDP encap. Make sure that TOS field of unencrypted packet is copied into TOS field of UDP encapsulated packet (outer header)
    
    3) changed IKE keepalives behavior slightly. The original code created an 'IKE runt' on Cisco ASA logs for every keep-alive sent, tested w/ 9.4(1). According to RFC 3948, the keep-alive payload should be 0xFF which is actually reflected as v1 in the code. Probably needs more thought but the 'v1 keep-alive' made the runt log entries on the ASA go away.
    
    This patch applies cleanly against the Ubuntu 14.04 vpnc source bundle vpnc_0.5.3r512-2ubuntu1.debian.tar.gz. I've tested this against production clusters of ASA headends and had no issues so far. YMMV.
    
    Thanks,
    -ralph
    
    References: https://bugs.launchpad.net/ubuntu/+source/vpnc/+bug/667963/comments/3
    References: http://lists.unix-ag.uni-kl.de/pipermail/vpnc-devel/2015-June/004163.html
    Applied-by: Daniel Lenski <dlenski@gmail.com>

--- src/vpnc.h
+++ src/vpnc.h
@@ -27,5 +27,7 @@ void process_late_ike(struct sa_block *s, uint8_t *r_packet, ssize_t r_length);
 void keepalive_ike(struct sa_block *s);
 void dpd_ike(struct sa_block *s);
 void print_vid(const unsigned char *vid, uint16_t len);
+void rekey_phase1(struct sa_block *s);
+
 
 #endif
